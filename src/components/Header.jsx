0a72cdadf7c60a6d0d4210318ad7e9e5e352785e
Kamiada <kamiada528@gmail.com>
2021-01-02 22:05:39 +0000

    initial set up

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './components.scss';
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                Dashboard
+            </header>
+        )
+    }
+}
+export default Header;
