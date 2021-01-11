cc2475ede00384d4268c4455ac28b34b9c8426de
Kamiada <kamiada528@gmail.com>
2021-01-11 21:23:23 +0000

    still figuring out how charts js works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Doughnut from './components/Doughnut';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Doughnut />
     </div>
   );
 }
---
+++ src/components/Doughnut.jsx
@@ -0,0 +1,39 @@
+import Chart from "chart.js";
+import React, { Component } from "react";
+import "./components.scss";
+class Doughnut extends Component {
+
+    chartRef = React.createRef();
+    
+    componentDidMount() {
+        const myChartRef = this.chartRef.current.getContext("2d");
+        
+        new Chart(myChartRef, {
+            type: "line",
+            data: {
+                //Bring in data
+                labels: ["Jan", "Feb", "March"],
+                datasets: [
+                    {
+                        label: "Sales",
+                        data: [86, 67, 91],
+                    }
+                ]
+            },
+            options: {
+                //Customize chart options
+            }
+        });
+    }
+    render() {
+        return (
+            <div>
+                <canvas
+                    id="myChart"
+                    ref={this.chartRef}
+                />
+            </div>
+        )
+    }
+}
+export default Doughnut;
