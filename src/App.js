59a1af99b94bcea3f4bf75cd987ca7401cc2e75c
Kamiada <kamiada528@gmail.com>
2021-01-13 20:43:39 +0000

    cleaning up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
 import Header from './components/Header';
 import Doughnut from './components/Doughnut';
 import Barchart from './components/Barchart';
 import RadarChart from './components/RadarChart';
 import BubbleChart from './components/BubbleChart';
 import Subpage from './components/Subpage';
+import "./components/components.scss";
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <Header />
       <Doughnut />
       <Barchart />
