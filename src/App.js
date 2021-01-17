ca116d899419bacfae005f4783baf22f7acf78ee
Kamiada <kamiada528@gmail.com>
2021-01-17 01:30:13 +0000

    it's time for API

--- src/App.js
+++ src/App.js
@@ -6,7 +6,19 @@ import RadarChart from "./components/RadarChart";
 import BubbleChart from "./components/BubbleChart";
 import Subpage from "./components/Subpage";
 import "./components/components.scss";
+
+const API = 'https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=nation;areaName=england&structure=%7B%22name%22:%22areaName%22%7D';
+
+function fetchData(api) {
+  return fetch(api)
+  .then(message =>
+    console.log(message)
+  )
+  .catch(error => console.error(error));
+}
+
 function App() {
+  fetchData(API);
   return (
     <div>
       <Header />
