019356d840044121a8ed8bcf10eca0f53ad8dca5
kamiada <kamiada528@gmail.com>
2021-01-28 18:13:26 +0000

    after long search over different npms packages for dropdowns, I may have finally found it. Or I just gave up

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import Barchart from "./components/Barchart";
 import Subpage from "./components/Subpage";
 import "./components/components.scss";
 import euStaticData from "./EUcovidData.json";
-import Dropdown from "react-dropdown";
+import Select from "react-select";
+import makeAnimated from "react-select/animated";
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -33,9 +35,9 @@ class App extends Component {
     });
     return results;
   };
-  handleChange = (selectedCountries) => {
-    this.setState({ selectedCountries }, () =>
-      console.log(`Option selected:`, this.state.selectedCountries)
+  handleChange = (selectedMonth) => {
+    this.setState({ selectedMonth }, () =>
+      console.log(`Option selected:`, this.state.selectedMonth)
     );
   };
   onSelect(selectedList, selectedItem) {}
@@ -43,31 +45,40 @@ class App extends Component {
   onRemove(selectedList, removedItem) {}
 
   render() {
-    const { selectedMonth } = this.state;
+    const { selectedMonth, selectedYear } = this.state;
+    const animatedComponents = makeAnimated();
     const months = [
-      { label: 'January', value: 1},
-      { label: 'February', value: 2},
-      { label: 'March', value: 3},
-      { label: 'April', value: 4},
-      { label: 'May', value: 5},
-      { label: 'June', value: 6},
-      { label: 'July', value: 7},
-      { label: 'August', value: 8},
-      { label: 'September', value: 9},
-      { label: 'October', value: 10},
-      { label: 'November', value: 11},
-      { label: 'December', value: 12},
+      { label: "January", value: 1 },
+      { label: "February", value: 2 },
+      { label: "March", value: 3 },
+      { label: "April", value: 4 },
+      { label: "May", value: 5 },
+      { label: "June", value: 6 },
+      { label: "July", value: 7 },
+      { label: "August", value: 8 },
+      { label: "September", value: 9 },
+      { label: "October", value: 10 },
+      { label: "November", value: 11 },
+      { label: "December", value: 12 },
     ];
+    const years = [{label: "2020", value: 2020}, {label: "2021", value:2021}];
     return (
       <div>
         <Header />
         <Subpage />
-        <Dropdown
-          className="dropdown"
+        <Select
+          closeMenuOnSelect={false}
+          components={animatedComponents}
+          defaultValue="months."
+          isMulti
           options={months}
-          value={selectedMonth}
-          placeholder="months."
-          onChange={console.log(selectedMonth)}
+        />
+        <Select
+          closeMenuOnSelect={false}
+          components={animatedComponents}
+          defaultValue="years."
+          isMulti
+          options={years}
         />
         <div className="container">
           {this.state.data && this.state.data.length > 0 ? (
